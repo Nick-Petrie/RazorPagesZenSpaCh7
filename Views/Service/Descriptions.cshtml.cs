c4e81dff72ef0050b43fd4431b42ec9d42dcdffe
Nick Petrie <nickolaspetrie@gmail.com>
2024-10-27 21:01:49 -0400

    Add project files.

---
+++ Views/Service/Descriptions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesZenSpaCh7.Views.Service
+{
+    public class DescriptionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
